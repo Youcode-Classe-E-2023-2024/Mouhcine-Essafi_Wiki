86eb25caa3dfc849a4282b6c725ef4f0625515c2
Mouhcine Essafi <messafi1337@gmail.com>
2024-01-09 14:47:06 +0100

    Update main.js
    add ajax for login

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,34 @@ window.addEventListener('DOMContentLoaded', () => {
         scrollPos = currentTop;
     });
 })
+
+const loginform = document.getElementById("loginForm");
+// console.log(loginform);
+if (loginform) {
+  loginform.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const formData = new FormData(this);
+
+    fetch("index.php?page=login", {
+      method: "POST",
+      body: formData
+    })
+      .then(response => response.text())
+      .then(data => {
+        console.log(data);
+
+        if (data.trim() === 'valide') {
+          console.log('Login successful');
+          // Redirect to the home page or perform other actions
+          window.location.href = "index.php?page=users";
+        } else {
+          console.log('Login failed');
+          // Handle failed login (e.g., display an error message)
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        // Handle errors here
+      });
+  });
+}
