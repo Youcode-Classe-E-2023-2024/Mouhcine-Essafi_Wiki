c51566ec07645e61253abf5cb6387afcc1c196ac
Mouhcine Essafi <messafi1337@gmail.com>
2024-01-10 16:39:06 +0100

    Update register_view.php

--- views/register_view.php
+++ views/register_view.php
@@ -3,15 +3,16 @@
         <div class="row justify-content-center">
             <div class="col-lg-6">
                 <h5 class="text-white text-center mt-5 mb-4">Create Your Account</h5>
-                <form>
-                    <div class="form-group mb-3">
-                        <input class="form-control py-4 w-100" type="text" placeholder="Full Name" required/>
+                <form id="register_Form">
+                    <div class="form-group mb-3" style="display: flex;">
+                        <input class="form-control py-4" type="text" placeholder="First Name" name="first_name" required />
+                        <input class="form-control py-4" type="text" placeholder="Last Name" name="last_name" required />
                     </div>
                     <div class="form-group mb-3">
-                        <input class="form-control py-4 w-100" type="email" placeholder="Email Address" required/>
+                        <input class="form-control py-4 w-100" type="email" placeholder="Email Address" name="email" required />
                     </div>
                     <div class="form-group mb-3">
-                        <input class="form-control py-4 w-100" type="password" placeholder="Password" required/>
+                        <input class="form-control py-4 w-100" type="password" placeholder="Password" name="password" required />
                     </div>
                     <div class="form-group mb-4">
                         <button class="btn btn-primary w-100" type="submit">Register</button>
@@ -21,6 +22,7 @@
                         <a href="index.php?page=login" class="text-primary font-weight-bold">Login here <i class="fas fa-angle-double-right"></i></a>
                     </div>
                 </form>
+
             </div>
         </div>
     </div>
