5235b7d6f572e6318ad526a8cc92fe4ff08ec12d
Mouhcine Essafi <messafi1337@gmail.com>
2024-01-10 10:19:55 +0100

    Update db.php
    add pdo to connection

--- _config/db.php
+++ _config/db.php
@@ -1,3 +1,12 @@
 <?php
 
-$db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// $db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+try {
+    $db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+    // Set PDO to throw exceptions on error
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Handle connection error
+    die("Error connecting to the database: " . $e->getMessage());
+}
